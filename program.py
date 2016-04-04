da3ba5e094f6e31abe6c6903b6c09f54db6bbc71
Trent Casey <trcasey@outlook.com>
2016-04-03 20:30:08 -0400

    Added different creature classes.

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 import time
 
-from actors import Wizard, Creature
+from actors import Wizard, SmallAnimal, Predator, Dragon
 import random
 
 def main():
@@ -18,11 +18,11 @@ def print_header():
 def game_loop():
 
     creatures = [
-        Creature('Toad', 1),
-        Creature('Tiger', 12),
-        Creature('Bat', 3),
-        Creature('Dragon', 50),
-        Creature('Evil Wizard', 1000),
+        SmallAnimal('Toad', 1),
+        Predator('Tiger', 12),
+        SmallAnimal('Bat', 3),
+        Dragon('Dragon', 50, 75, True),
+        Wizard('Evil Wizard', 1000),
     ]
 
     hero = Wizard('Gandolf', 75)
@@ -49,6 +49,11 @@ def game_loop():
                 print(' * A {} of level {}'.format(c.name, c.level))
         else:
             print("OK, exiting game... bye!")
+            break
+
+
+        if not creatures:
+            print('You defeated all the creatures, well done!')
             break
 
 if __name__ == '__main__':
